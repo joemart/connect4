cd19d0d2efa2f16ece1fcb86d5074ad2b7940fb4
Joseph Martire <joemart06@gmail.com>
2024-05-15 14:07:53 -0400

    Add firebase config; add baseurl; add firebase;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Board from "./Board";
+import "config/firebase"
 
 export default function Home() {
+
+
   return (
     <>
       Connect 4
       <Board>
         asdf
+
+
       </Board>
     </>
   );
