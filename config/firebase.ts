24f3368a4dac065488b0fcc4918e3eebf9b3456d
Joseph Martire <joemart06@gmail.com>
2024-10-21 16:32:13 -0400

    Add firebase NEXT_PUBLIC_ prefix to the config

--- config/firebase.ts
+++ config/firebase.ts
@@ -12,27 +12,36 @@ import {getDatabase} from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const dev = process.env.NODE_ENV !== 'production';
-const firebaseConfig = dev? {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain:process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-  databaseURL: "https://connect-4-94fab-default-rtdb.firebaseio.com/"
-} : {
-  apiKey: process.env.API_KEY,
-  authDomain:process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-  databaseURL: "https://connect-4-94fab-default-rtdb.firebaseio.com/"
-};
-console.log(dev)
-console.log(process.env)
+// const firebaseConfig = dev? {
+//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+//   authDomain:process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+//   databaseURL: "https://connect-4-94fab-default-rtdb.firebaseio.com/"
+// } : {
+//   apiKey: process.env.API_KEY,
+//   authDomain:process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+//   measurementId: process.env.MEASUREMENT_ID,
+//   databaseURL: "https://connect-4-94fab-default-rtdb.firebaseio.com/"
+// };
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain:process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    databaseURL: "https://connect-4-94fab-default-rtdb.firebaseio.com/"
+  }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
