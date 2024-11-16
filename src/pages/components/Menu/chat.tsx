ce5665a19a294e8828c37a42262c7afbf85d7c85
Joseph Martire <joemart06@gmail.com>
2024-11-16 14:25:50 -0400

    Add auto scroll to menu; refine menu scss

--- src/pages/components/Menu/chat.tsx
+++ src/pages/components/Menu/chat.tsx
@@ -1,7 +1,7 @@
 import styles from "./chat.module.scss"
 import Person from "@/../public/person.svg"
 import Send from "@/../public/send.svg"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState, useRef } from "react"
 import { OnValueDB, PushDB } from "@/utils/DBClass"
 import { AuthContext } from "../Context/AuthContext"
 
@@ -13,6 +13,12 @@ const Chat = () => {
 
     const Auth = useContext(AuthContext)
     const [onlineUsers, setOnlineUsers] = useState<number>(0)
+    const chatRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (chatRef.current)
+            chatRef.current.scrollIntoView({ behavior: "smooth", block: "end" })
+    }, [chat])
 
     useEffect(() => {
         return OnValueDB.chatOnValue(snapshot => setChat(snapshot.val()))
@@ -43,7 +49,7 @@ const Chat = () => {
             </div>
         </div>
         <div className={styles["chat_wrapper"]}>
-            <div className={styles["chat"]}>
+            <div className={styles["chat"]} ref={chatRef}>
                 {chat ? Object.values(chat).map(c => <div>{c.user} : {c.message}</div>) : <></>}
             </div>
         </div>
